6017201a58b569afe777a44f7d6af52d7c9edc00
rj-robinjoseph <robinjoseph452@gmail.com>
2022-07-10 21:40:15 +0530

    Footer Added

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Categories from "./components/Categories";
 import Deals from "./components/Deals";
 import Featured from "./components/Featured";
 import Features from "./components/Features";
+import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Section from "./components/Section";
 
@@ -17,6 +18,7 @@ function App() {
       <Deals />
       <Categories />
       <Features />
+      <Footer />
     </div>
   );
 }

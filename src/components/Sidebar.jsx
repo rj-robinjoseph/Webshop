6a8c4dacf225a6fea42bfa6e9eae41ca7bc16497
rj-robinjoseph <robinjoseph452@gmail.com>
2022-07-13 22:39:42 +0530

    Sidebar added in Products Page.

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "../styles/Sidebar.scss";
+
+function Sidebar() {
+  return <div className="sidebar"></div>;
+}
+
+export default Sidebar;
--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import CategoryContainer from "../components/CategoryContainer";
 import Header from "../components/Header";
+import Sidebar from "../components/Sidebar";
 
 function Products() {
   return (
     <div>
       <Header />
       <CategoryContainer />
+      <Sidebar />
     </div>
   );
 }

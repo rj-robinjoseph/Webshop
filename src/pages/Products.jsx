26d9a3bc7ad36f429f6b45db8bdbc43762ee9a0a
rj-robinjoseph <robinjoseph452@gmail.com>
2022-07-16 23:23:53 +0530

    Updating Sidebar.

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,13 +1,16 @@
 import React from "react";
 import CategoryContainer from "../components/CategoryContainer";
 import Header from "../components/Header";
+import Footer from "../components/Footer";
 import Sidebar from "../components/Sidebar";
+import "../styles/Product.scss";
+import "../styles/_global.scss";
 
 function Products() {
   return (
     <div>
       <Header />
-      <CategoryContainer />
+      {/* <CategoryContainer /> */}
       <Sidebar />
     </div>
   );
